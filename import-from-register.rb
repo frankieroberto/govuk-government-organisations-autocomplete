201b91f7d06d75bcb2c412087a3ded0af08dc6b5
Frankie Roberto <frankie@frankieroberto.com>
2018-01-25 14:04:08 +0000

    Update register-import script
    Merge changes instead of over-writing.

--- import-from-register.rb
+++ import-from-register.rb
@@ -47,16 +47,39 @@ grouped_by_key.each_pair.collect do |key, values|
   all_names = values.collect {|value| value[:data]['name'] }.uniq
 
   final << {
-    key: key,
-    current_name: current_name,
-    other_names: all_names - [current_name],
-    start_date: current_data['start-date'],
-    end_date: current_data['end-date']
+    'key' => key,
+    'current_name' => current_name,
+    'other_names' => all_names - [current_name],
+    'start_date' => current_data['start-date'],
+    'end_date' => current_data['end-date']
   }
 end
 
+json_file = File.open('data.json', 'r')
+
+existing_orgs = JSON.parse(json_file.read)
+
+final.each do |register_org|
+
+  existing_org = existing_orgs.detect {|org| org['key'] == register_org['key'] }
+
+  if existing_org.nil?
+    # puts "FOUND ONE:" + register_org['current_name']
+    existing_org = register_org
+    existing_orgs << existing_org
+  end
+
+  existing_org['current_name'] = register_org['current_name']
+  existing_org['other_names'] = (existing_org['other_names'] + register_org['other_names']).uniq.sort
+  existing_org['start_date'] = register_org['start_date']
+  existing_org['end_date'] = register_org['end_date'] unless register_org['end_date'].nil?
+
+end
+
+json_file.close
+
 json_file = File.open('data.json', 'w')
 
-json_file.write JSON.pretty_generate(final.sort_by {|org| org[:key]})
+json_file.write JSON.pretty_generate(existing_orgs.sort_by {|org| org['key']})
 
 json_file.close
